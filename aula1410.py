fce8510851312b7a8f69363221ad35e941270b1b
Treinamentos <treinamento@cfp107_2.local>
2025-10-14 21:50:39 -0300

    Aula do dia 14 out

---
+++ aula1410.py
@@ -0,0 +1,58 @@
+#print (2+ 3 ** 2 * 2 ) #Exponenciação tem procedencia maior
+"""
+# Um input recebe string por padrao
+nome = input("Digite seu nome:   ")
+sobrenome = input("Digite seu sobrenome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+imc = peso/(altura*altura)
+print ("Ola, ", nome, sobrenome)
+print("Voce tem", idade, "anos")
+print("Seu IMC é de ", round(imc,2))
+
+"""
+
+
+
+"""
+
+n1 = int(input("Digite o valor 1:  "))
+n2 = int(input("Digite o valor 2: "))
+soma = n1 + n2 
+print(str(soma))
+
+"""
+"""
+n1 = int(input("Digite o valor 1:  "))
+n2 = int(input("Digite o valor 2: "))
+
+print("Soma : ", n1+n2)
+
+n1 = int(input("Digite o valor 1:  "))
+n2 = int(input("Digite o valor 2: "))
+
+print("Multiplicacao: ", n1*n2)
+
+n1 = int(input("Digite o valor 1:  "))
+n2 = int(input("Digite o valor 2: "))
+
+print("Divisao {:.2f}" .format(n1/n2))
+
+print(f"{n1} + {n2 =} ",n1 - n2)
+
+"""
+nome = input("Digite seu nome:   ")
+sobrenome = input("Digite seu sobrenome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+imc = peso/(altura*altura)
+
+print("Ola,", nome,sobrenome,". Voce tem", idade, "anos." " Voce pesa", peso, "Kg.", "Altura é de ", altura)
+
+print(f"Ola {nome} {sobrenome}, voce tem {idade} anos. Você pesa {peso} kg. Altura de {altura}m. Seu IMC é de: {imc:.2f}")
+
+
+
+
